0af0503c8b7310d701ad47f58ef3db2d94051df6
Alexisxde <olivarezalexis749@gmail.com>
2025-03-04 09:38:11 -0300

    chore: Update Astro configuration to include trailingSlash and build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,11 @@ import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
 import { defineConfig } from "astro/config"
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: { alias: { "@": path.resolve(__dirname, "src") } }
-	},
+	trailingSlash: "never",
+	build: { format: "file" },
+	vite: { plugins: [tailwindcss()] },
 	integrations: [react(), icon()]
 })
