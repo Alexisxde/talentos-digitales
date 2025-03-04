8be8995c278c5862ae3fc6b6cfd418762abe621c
Alexisxde <olivarezalexis749@gmail.com>
2025-03-04 09:27:14 -0300

    chore: Update Astro configuration to include fileURLToPath and __dirname

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
 import { defineConfig } from "astro/config"
 import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
